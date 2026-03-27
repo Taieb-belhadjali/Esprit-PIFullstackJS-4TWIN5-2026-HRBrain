dc7d3cd5755a8f9c7602b536708dbef95f4e9503
Taieb-belhadjali <131562821+Taieb-belhadjali@users.noreply.github.com>
2026-03-27 20:15:08 +0100

    Update CORS configuration to allow all origins

--- BackOffice/src/main.ts
+++ BackOffice/src/main.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
